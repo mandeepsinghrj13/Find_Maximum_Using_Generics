4fb910d5a0965615835675323b35086b22808f9b
mandeep <singh.mr.1996@gmail.com>
2021-06-30 17:32:31 +0530

    given 3 string find the maximum

--- src/com/bridgelabz/findMaximum.java
+++ src/com/bridgelabz/findMaximum.java
@@ -2,9 +2,9 @@ package com.bridgelabz;
 public class findMaximum 
 {
 	
-	public static void biggesOfThree(Float a, Float b, Float c)
+	public static void biggesOfThree(String a, String b, String c)
 	{
-		Float max = a; // assume x is initially the largest
+		String max = a; // assume x is initially the largest
 
 	    if (b.compareTo(max) > 0)
 	      max = b; // y is the largest so far
@@ -18,9 +18,9 @@ public class findMaximum
 	public static void main(String[] args) 
 	{		
 		System.out.println("Welcome to maximum generics problems");
-		Float a = 10.10f;
-		Float b = 2000.12312f;
-		Float c = 300.300f;
+		String a = "chandan";
+		String b = "mandeep";
+		String c = "kuldeep";
 		biggesOfThree (a,b,c);
 	}
 
